2e6357e9b6f3877412eb1dcaabd3f1210cebbeca
ViktorijaRosa <63238039+ViktorijaRosa@users.noreply.github.com>
2020-04-07 09:10:37 -0400

    Log multi line support and ability to remove packages

--- chocofrontend.ps1
+++ chocofrontend.ps1
@@ -13,7 +13,12 @@ $Button_Click2 =
             $TextBox2.Text = choco upgrade all
             [System.Windows.MessageBox]::Show('All packages have been updated')
 }
-
+$Button_Click3 = 
+{
+            $x = $TextBox1.Text
+            $TextBox2.Text = choco uninstall $x
+            [System.Windows.MessageBox]::Show('Package removed: ' +$x)
+}
 
 
 [System.Windows.Forms.Application]::EnableVisualStyles()
@@ -27,9 +32,18 @@ $Button1                         = New-Object system.Windows.Forms.Button
 $Button1.text                    = "Install Package"
 $Button1.width                   = 68
 $Button1.height                  = 40
-$Button1.location                = New-Object System.Drawing.Point(166,49)
+$Button1.location                = New-Object System.Drawing.Point(200,95)
 $Button1.Font                    = 'Microsoft Sans Serif,10'
 $Button1.Add_Click($Button_Click)
+
+$Button3                         = New-Object system.Windows.Forms.Button
+$Button3.text                    = "Remove Package"
+$Button3.width                   = 68
+$Button3.height                  = 40
+$Button3.location                = New-Object System.Drawing.Point(130,95)
+$Button3.Font                    = 'Microsoft Sans Serif,10'
+$Button3.Add_Click($Button_Click3)
+
 
 $Button2                         = New-Object system.Windows.Forms.Button
 $Button2.text                    = "Upgrade all"
@@ -43,18 +57,18 @@ $TextBox1                        = New-Object system.Windows.Forms.TextBox
 $TextBox1.multiline              = $false
 $TextBox1.width                  = 100
 $TextBox1.height                 = 20
-$TextBox1.location               = New-Object System.Drawing.Point(149,95)
+$TextBox1.location               = New-Object System.Drawing.Point(149,49)
 $TextBox1.Font                   = 'Microsoft Sans Serif,10'
 
 
 $TextBox2                        = New-Object system.Windows.Forms.TextBox
 $TextBox2.multiline              = $true
 $TextBox2.width                  = 216
-$TextBox2.height                 = 20
+$TextBox2.height                 = 140
 $TextBox2.location               = New-Object System.Drawing.Point(94,160)
 $TextBox2.Font                   = 'Microsoft Sans Serif,10'
 
-$Form.controls.AddRange(@($Button1,$Button2,$TextBox1,$TextBox2))
+$Form.controls.AddRange(@($Button1,$Button2,$TextBox1,$TextBox2,$Button3))
 
 
 $result = $form.ShowDialog()
